6fbe0dad8b649f11fe704a9ebf458dad7eec5cec
Timothy Washington <twashing@interruptsoftware.com>
2020-07-15 19:07:57 -0400

    Workbench code to
      Bump tick-sleep (up or down)
      Handle control, game and timer events
        :timeout :exit
        :win :lose :next-tick
    
      Stream messages
        :timer :timeout [:pause :resume] :exit :win :lose

--- dev/user.clj
+++ dev/user.clj
@@ -82,6 +82,20 @@
      :remaining-in-minutes (t/in-minutes interval)
      :remaining-in-seconds (rem (t/in-seconds interval) 60)}))
 
+(defn pause-game? [pause?]
+
+  ;; TODO stream a :game-event
+  (reset! paused? pause?))
+
+(defn handle-control-event [control remaining]
+
+  :exit :win :lose :timeout
+
+  ;; TODO stream
+  ;; :game-event
+  ;; :timer-event
+  )
+
 (defn stream-game! [control-channel input-channel pause-atom tick-sleep-atom level-timer-atom]
 
   (let [start (t/now)]
@@ -93,21 +107,35 @@
             [controlv ch] (core.async/alts! [(core.async/timeout @tick-sleep-atom)
                                       control-channel])]
 
+        ;; pause
+        ;; control
+        ;; timer
         (match [@pause-atom controlv (time-expired? remaining)]
 
-               [true :exit _] (println (format "< Paused > %s / Exiting..." (format-remaining-time remaining)))
+               [true :exit _] (do
+                                (handle-control-event controlv remaining)
+                                (println (format "< Paused > %s / Exiting..." (format-remaining-time remaining))))
                [true _ _] (let [new-end (t/plus end (t/seconds 1))
                                 remaining (calculate-remaining-time now new-end)]
                             (println (format "< Paused > %s" (format-remaining-time remaining)))
                             (recur (t/now) new-end))
 
-               [_ :exit _] (println (format "Running %s / Exiting" (format-remaining-time remaining)))
-               [_ :win _] (println (format "Win %s" (format-remaining-time remaining)))
-               [_ :lose _] (println (format "Lose %s" (format-remaining-time remaining)))
+               [_ :exit _] (do (handle-control-event controlv remaining)
+                               (println (format "Running %s / Exiting" (format-remaining-time remaining))))
+               [_ :win _] (do (handle-control-event controlv remaining)
+                              (println (format "Win %s" (format-remaining-time remaining))))
+               [_ :lose _] (do (handle-control-event controlv remaining)
+                               (println (format "Lose %s" (format-remaining-time remaining))))
                [_ _ false] (let [v (<! input-channel)]
+
+                             ;; TODO stream
+                             ;; :stock-tick
+                             ;; :timer-event
+
                              (println (format "Running %s / %s" (format-remaining-time remaining) v))
                              (recur  (t/now) end))
-               [_ _ true] (println (format "Running %s / TIME'S UP!!" (format-remaining-time remaining))))))))
+               [_ _ true] (do (handle-control-event :timeout remaining)
+                              (println (format "Running %s / TIME'S UP!!" (format-remaining-time remaining)))))))))
 
 
 (comment
@@ -157,27 +185,30 @@
   (stream-game! control-channel input-channel paused? tick-sleep-atom level-timer-atom)
 
 
-  (reset! paused? true)
-  (reset! paused? false)
+  ;; :pause :resume
+  (pause-game? true)
+  (pause-game? false)
+
+
+  ;; bump tick sleep (up or down)
+  (reset! tick-sleep-atom 500)
+  (reset! tick-sleep-atom 1000)
+  (reset! tick-sleep-atom 1500)
+
+  ;; :win :lose :exit
   (core.async/>!! control-channel :exit)
   (core.async/>!! control-channel :win)
   (core.async/>!! control-channel :lose)
+
 
   ;; NOTE
   :timeout :exit
   :win :lose :next-tick
 
-  ;; TODO
   ;; Stream messages
-  :timer :exit :win :lose
-
-  ;; Bump timer (up or down)
-  ;; Bump tick-sleep (up or down)
+  :timer :timeout [:pause :resume] :exit :win :lose
 
 
-  (def start (t/now))
-  (def end (t/plus start (t/minutes 5)))
-  ;; now
-  ;; remaining (t/in-seconds (t/interval 'now end))
-
+  ;; TODO Maybe later
+  ;; [x] Bump timer (up or down)
   )
